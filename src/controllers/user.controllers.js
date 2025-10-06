49e9a6a6423d37d7fc884583e50d36f9ab343d48
vishaljati <jativishal120@gmail.com>
2025-10-06 12:13:04 +0530

    Add getUserChannelProfile function: implement channel retrieval with subscriber details and validation

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -401,6 +401,97 @@ const updateUserCoverImage = asyncHandler (async(req,res)=>{
 
 
 })
+
+
+const getUserChannelProfile = asyncHandler(async (req,res) => {
+    const { username }= req.params
+    if (!username?.trim()) {
+        throw new ApiError(400,"Username Invalid"); 
+    }
+ 
+     // mongodb aggregation piprline
+    const channel = User.aggregate([
+           {
+            $match:{
+                 username: username?.toLowerCase()
+            }
+         },
+         {
+           $lookup:{
+              from : "subscriptions",
+              localField:"_id",
+              foreignField:"channel",
+              as:"subscribers"
+           }
+         },
+         {
+           $lookup:{
+              from : "subscriptions",
+              localField:"_id",
+              foreignField:"subscriber",
+              as:"followed"
+           }
+         },
+         {
+           $addFields:{
+
+               subscriberCount : {
+                  $size:"$subscribers"
+               },
+               followedCount :{
+                 $size: "$followed"
+               },
+               isSubscribed :{
+                  $cond : {
+                     if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                     then: true,
+                     else: false
+                  }
+               }
+
+           }
+         },
+         {
+            $project:{
+                 
+                fullName: 1 ,
+                username: 1,
+                avatar : 1,
+                coverImage: 1,
+                subscriberCount: 1,
+                followedCount : 1,
+                isSubscribed :1 ,
+                email : 1,
+                createdAt : 1
+                 
+            }
+         }
+
+
+
+
+    ])
+    console.log(channel); // check 
+
+    if (!channel?.length) {
+        throw new ApiError(404,"Channel is not exist");
+        
+    }
+
+    return res
+    .status(200)
+    .json( new ApiResponse(
+        200, channel[0],"User channel fetched successfully"
+    ))
+
+    
+
+   
+
+ })
+
+
+
 export {
     registerUser,
     loginUser,
@@ -410,5 +501,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
 }
