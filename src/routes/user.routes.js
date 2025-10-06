4bc212ceaf41c41305f49fce759d8f527c4121cb
vishaljati <jativishal120@gmail.com>
2025-10-06 22:09:00 +0530

    Refactor user routes: add missing route handlers for change password, current user, account update, avatar, and cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,14 @@ import { Router } from "express";
 import { registerUser ,
          loginUser ,
          logOutUser,
-         refreshAccessToken,   } from "../controllers/user.controllers.js";
+         refreshAccessToken, 
+         changeCurrentPassword,
+         getCurrentUser,
+         updateAccountDetails,
+         updateUserAvatar,
+         updateUserCoverImage,
+         getUserChannelProfile,
+         getWatchHistory, } from "../controllers/user.controllers.js";
 import  { upload } from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js"
 
@@ -27,8 +34,14 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT ,  upload.single("avatar") , updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT ,  upload.single("coverImage") , updateUserCoverImage)
 
-
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 
