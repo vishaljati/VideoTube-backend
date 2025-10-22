516d92b1057d4592d5bd54104f057d125b1b578e
vishaljati <jativishal120@gmail.com>
2025-10-22 12:22:39 +0530

    Refactor app.js: remove unnecessary comment and clean up code formatting Enhance index.js: add logging of connection instance for better debugging

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,8 @@ import {DB_NAME} from "../constant.js";
  const connectDB = async () => {
       try {
          const connectionInstant = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+         console.log(connectionInstant);
+         
          console.log("\n MONGODB Connected Successfully!! \n DB HOST :" ,`${connectionInstant.connection.host}`);
          
          
