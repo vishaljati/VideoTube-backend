a64f0bc4a7ef7ab234f88b120f95592365506cc9
vishaljati <jativishal120@gmail.com>
2025-10-26 12:47:14 +0530

    Refactor ApiResponse constructor: improve formatting and readability

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,10 +1,14 @@
 class ApiResponse{
-    constructor(statuscode,data,message="Success"){
-        this.statusCode = statuscode
-        this.data = data
-        this.message = message
-        this.success = statuscode < 400
-    }
+    constructor(
+            statuscode,
+            data,
+            message="Success"
+        ){
+            this.statusCode = statuscode
+            this.data = data
+            this.message = message
+            this.success = statuscode < 400
+        }
 }
 
 export { ApiResponse }
