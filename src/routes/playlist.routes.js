1611099fac540a64dda033493ed7fc1340f57aa1
vishaljati <jativishal120@gmail.com>
2025-10-28 00:08:52 +0530

    Add playlist controller and route basic structure

---
+++ src/routes/playlist.routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import {
+    addVideoToPlaylist,
+    createPlaylist,
+    deletePlaylist,
+    getPlaylistById,
+    getUserPlaylists,
+    removeVideoFromPlaylist,
+    updatePlaylist,
+} from "../controllers/playlist.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+// routes
+
+export default router;
